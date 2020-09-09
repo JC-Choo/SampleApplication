8fec2679362fdf3c1c65c30726d113971fc6ef93
mustit-devcjc <dev_cjc@mustit.co.kr>
2020-09-09 11:13:37 +0900

    Test selection recyclerview
    Signed-off-by: mustit-devcjc <dev_cjc@mustit.co.kr>

--- app/src/main/java/dev/chu/memo/ui/rv_simple/RvSimpleActivity.kt
+++ app/src/main/java/dev/chu/memo/ui/rv_simple/RvSimpleActivity.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import dev.chu.memo.R
 import dev.chu.memo.base.BaseActivity2
 import dev.chu.memo.databinding.ActivityRvSimpleBinding
-import dev.chu.memo.databinding.ItemRvSimpleBinding
-import dev.chu.memo.etc.extension.showToast
 import dev.chu.memo.ui.rv_simple.amimation.usage.Post
 import dev.chu.memo.ui.rv_simple.amimation.usage.PostsAdapter
+import java.util.*
 
 /**
  * 참고 : https://medium.com/@jermainedilao/powerful-and-reusable-listadapter-using-databinding-for-simple-lists-55ac377fa750
@@ -17,14 +16,21 @@ class RvSimpleActivity : BaseActivity2<ActivityRvSimpleBinding>(R.layout.activit
 
     override fun initView(savedInstanceState: Bundle?) {
         binding.list.apply {
-            adapter = SimpleListAdapter<Article, ItemRvSimpleBinding>(
-                ArticleDiffUtil(),
-                R.layout.item_rv_simple
-            ) { item ->
-                showToast("item = ${item.title}")
-            }.apply {
-                submitList(getItems())
+//            adapter = SimpleListAdapter<Article, ItemRvSimpleBinding>(
+//                ArticleDiffUtil(),
+//                R.layout.item_rv_simple
+//            ) { item ->
+//                showToast("item = ${item.title}")
+//            }.apply {
+//                submitList(getItems())
+//            }
+
+
+            val selectAdapter = SelectAdapter().apply {
+                list = createRandomIntList()
             }
+            adapter = selectAdapter
+            selectAdapter.notifyDataSetChanged()
         }
 
         binding.postsRecycler.apply {
@@ -32,6 +38,11 @@ class RvSimpleActivity : BaseActivity2<ActivityRvSimpleBinding>(R.layout.activit
                 submitList(createPosts())
             }
         }
+    }
+
+    private fun createRandomIntList(): List<Int> {
+        val random = Random()
+        return (1..10).map { random.nextInt() }
     }
 
     private fun createPosts(): List<Post> =
