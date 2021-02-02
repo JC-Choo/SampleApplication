a02302a7ce4b62467b38628c4f2a690fe7852a3f
JC-Choo <jicheolchoo@gmail.com>
2021-02-02 13:54:39 +0900

    Add module named timer
    Signed-off-by: JC-Choo <jicheolchoo@gmail.com>

--- timer/src/main/java/dev/chu/timer/MainActivity.kt
+++ timer/src/main/java/dev/chu/timer/MainActivity.kt
@@ -1,11 +1,119 @@
 package dev.chu.timer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import dev.chu.timer.Utility.getFormattedStopWatch
+import dev.chu.timer.databinding.ActivityMainBinding
 
+/**
+ * 참고 : https://medium.com/swlh/how-to-create-a-stopwatch-in-android-117912264491
+ */
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    private val mInterval = TIMER_INTERVAL
+    private var mHandler: Handler? = null
+    private var timeInSeconds = 0L
+    private var startButtonClicked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        initStopWatch()
+
+        binding!!.resetButton.setOnClickListener {
+            stopTimer()
+            resetTimerView()
+        }
+    }
+
+    private fun initStopWatch() {
+        binding?.textViewStopWatch?.text = getString(R.string.init_stop_watch_value)
+    }
+
+    private fun resetTimerView() {
+        timeInSeconds = 0
+        startButtonClicked = false
+        binding?.startOrStopTextView?.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                R.color.teal_700
+            )
+        )
+        binding?.startOrStopTextView?.setText(R.string.start)
+        initStopWatch()
+    }
+
+    fun startOrStopButtonClicked(v: View) {
+        if (!startButtonClicked) {
+            startTimer()
+            startTimerView()
+        } else {
+            stopTimer()
+            stopTimerView()
+        }
+    }
+
+    private fun stopTimerView() {
+        binding?.startOrStopTextView?.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                R.color.teal_700
+            )
+        )
+        binding?.startOrStopTextView?.setText(R.string.resume)
+        startButtonClicked = false
+    }
+
+    private fun startTimerView() {
+        binding?.startOrStopTextView?.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                R.color.red
+            )
+        )
+        binding?.startOrStopTextView?.setText(R.string.stop)
+        startButtonClicked = true
+    }
+
+    private fun startTimer() {
+        mHandler = Handler(Looper.getMainLooper())
+        mStatusChecker.run()
+    }
+
+    private fun stopTimer() {
+        mHandler?.removeCallbacks(mStatusChecker)
+    }
+
+    private var mStatusChecker: Runnable = object : Runnable {
+        override fun run() {
+            try {
+                timeInSeconds += 1
+                Log.e("timeInSeconds", timeInSeconds.toString())
+                updateStopWatchView(timeInSeconds)
+            } finally {
+                // 100% guarantee that this always happens, even if
+                // your update method throws an exception
+                mHandler!!.postDelayed(this, mInterval.toLong())
+            }
+        }
+    }
+
+    private fun updateStopWatchView(timeInSeconds: Long) {
+        val formattedTime = getFormattedStopWatch((timeInSeconds * 1000))
+        Log.e("formattedTime", formattedTime)
+        binding?.textViewStopWatch?.text = formattedTime
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopTimer()
+        mHandler = null
     }
 }
--- timer/src/main/java/dev/chu/timer/Utility.kt
+++ timer/src/main/java/dev/chu/timer/Utility.kt
@@ -1,4 +1,21 @@
 package dev.chu.timer
 
-class Utility {
+import java.util.concurrent.TimeUnit
+
+const val TIMER_INTERVAL = 1000
+
+object Utility {
+
+    fun getFormattedStopWatch(ms: Long): String {
+        var milliseconds = ms
+        val hours = TimeUnit.MILLISECONDS.toHours(milliseconds)
+        milliseconds -= TimeUnit.HOURS.toMillis(hours)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(milliseconds)
+        milliseconds -= TimeUnit.MINUTES.toMillis(minutes)
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds)
+
+        return "${if (hours < 10) "0" else ""}$hours:" +
+                "${if (minutes < 10) "0" else ""}$minutes:" +
+                "${if (seconds < 10) "0" else ""}$seconds"
+    }
 }
