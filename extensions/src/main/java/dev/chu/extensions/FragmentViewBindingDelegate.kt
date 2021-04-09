3f7379a972d29be28fa729c20349cd4747b0747e
JC-Choo <jicheolchoo@gmail.com>
2021-04-09 10:14:09 +0900

    Unused fragment delegate and use activity delegate
    Signed-off-by: JC-Choo <jicheolchoo@gmail.com>

---
+++ extensions/src/main/java/dev/chu/extensions/FragmentViewBindingDelegate.kt
@@ -0,0 +1,56 @@
+//package dev.chu.extensions
+//
+//import android.view.View
+//import androidx.fragment.app.Fragment
+//import androidx.lifecycle.DefaultLifecycleObserver
+//import androidx.lifecycle.LifecycleOwner
+//import androidx.viewbinding.ViewBinding
+//import kotlin.properties.ReadOnlyProperty
+//import kotlin.reflect.KProperty
+//
+//class FragmentViewBindingDelegate<T : ViewBinding>(
+//    private val bindingClass: Class<T>,
+//    private val fragment: Fragment
+//) : ReadOnlyProperty<Fragment, T> {
+//
+//    private var binding: T? = null
+//
+//    init {
+//        fragment.lifecycle.addObserver(object : DefaultLifecycleObserver {
+//            override fun onCreate(owner: LifecycleOwner) {
+//                fragment.viewLifecycleOwnerLiveData.observe(fragment) { viewLifecycleOwner ->
+//                    viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
+//                        override fun onDestroy(owner: LifecycleOwner) {
+//                            binding = null
+//                        }
+//                    })
+//                }
+//            }
+//        })
+//
+////        fragment.lifecycle.addObserver(object : LifecycleObserver {
+////            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+////            fun onCreate() {
+////                fragment.viewLifecycleOwnerLiveData.observe(fragment, { viewLifecycleOwner ->
+////                    viewLifecycleOwner.lifecycle.addObserver(object : LifecycleObserver {
+////                        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+////                        fun onDestroy() {
+////                            binding = null
+////                        }
+////                    })
+////                })
+////            }
+////        })
+//    }
+//
+//    @Suppress("UNCHECKED_CAST")
+//    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+//        if (binding == null)
+//            binding = bindingClass.getMethod("bind", View::class.java)
+//                .invoke(null, thisRef.requireView()) as T
+//        return binding!!
+//    }
+//}
+//
+//inline fun <reified T : ViewBinding> Fragment.viewBinding() =
+//    FragmentViewBindingDelegate(T::class.java, this)
--- rv_checkbox_selection/src/main/java/dev/chu/rv_checkbox_selection/MainActivity.kt
+++ rv_checkbox_selection/src/main/java/dev/chu/rv_checkbox_selection/MainActivity.kt
@@ -13,34 +13,39 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+//    private val binding by viewBinding {
+//        ActivityMainBinding.inflate(it)
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+//        setContentView(binding.root)
 
-        val list = listOf(RowModel(RowType.TopHeader, "", "" , false),
-            RowModel(RowType.CatHeader, "", "Vegetables" , false),
-            RowModel(RowType.ProductRow, "Tomato", "Vegetables" , false),
-            RowModel(RowType.ProductRow, "Cucumber", "Vegetables" , false),
-            RowModel(RowType.ProductRow, "Pepper", "Vegetables" , false),
-            RowModel(RowType.CatHeader, "", "Fruit" , false),
-            RowModel(RowType.ProductRow, "Apple", "Fruit" , false),
-            RowModel(RowType.ProductRow, "Banana", "Fruit" , false),
+        val list = listOf(
+            RowModel(RowType.TopHeader, "", "", false),
+            RowModel(RowType.CatHeader, "", "Vegetables", false),
+            RowModel(RowType.ProductRow, "Tomato", "Vegetables", false),
+            RowModel(RowType.ProductRow, "Cucumber", "Vegetables", false),
+            RowModel(RowType.ProductRow, "Pepper", "Vegetables", false),
+            RowModel(RowType.CatHeader, "", "Fruit", false),
+            RowModel(RowType.ProductRow, "Apple", "Fruit", false),
+            RowModel(RowType.ProductRow, "Banana", "Fruit", false),
 
-            RowModel(RowType.CatHeader, "", "Vegetables2" , false),
-            RowModel(RowType.ProductRow, "Tomato", "Vegetables2" , false),
-            RowModel(RowType.ProductRow, "Cucumber", "Vegetables2" , false),
-            RowModel(RowType.ProductRow, "Pepper", "Vegetables2" , false),
-            RowModel(RowType.CatHeader, "", "Fruit2" , false),
-            RowModel(RowType.ProductRow, "Apple", "Fruit2" , false),
-            RowModel(RowType.ProductRow, "Banana", "Fruit2" , false),
+            RowModel(RowType.CatHeader, "", "Vegetables2", false),
+            RowModel(RowType.ProductRow, "Tomato", "Vegetables2", false),
+            RowModel(RowType.ProductRow, "Cucumber", "Vegetables2", false),
+            RowModel(RowType.ProductRow, "Pepper", "Vegetables2", false),
+            RowModel(RowType.CatHeader, "", "Fruit2", false),
+            RowModel(RowType.ProductRow, "Apple", "Fruit2", false),
+            RowModel(RowType.ProductRow, "Banana", "Fruit2", false),
 
-            RowModel(RowType.CatHeader, "", "Vegetables3" , false),
-            RowModel(RowType.ProductRow, "Tomato", "Vegetables3" , false),
-            RowModel(RowType.ProductRow, "Cucumber", "Vegetables3" , false),
-            RowModel(RowType.ProductRow, "Pepper", "Vegetables3" , false),
-            RowModel(RowType.CatHeader, "", "Fruit3" , false),
-            RowModel(RowType.ProductRow, "Apple", "Fruit3" , false),
-            RowModel(RowType.ProductRow, "Banana", "Fruit3" , false)
+            RowModel(RowType.CatHeader, "", "Vegetables3", false),
+            RowModel(RowType.ProductRow, "Tomato", "Vegetables3", false),
+            RowModel(RowType.ProductRow, "Cucumber", "Vegetables3", false),
+            RowModel(RowType.ProductRow, "Pepper", "Vegetables3", false),
+            RowModel(RowType.CatHeader, "", "Fruit3", false),
+            RowModel(RowType.ProductRow, "Apple", "Fruit3", false),
+            RowModel(RowType.ProductRow, "Banana", "Fruit3", false)
         )
 
         val adapter = CheckBoxAdapter(this, list)
