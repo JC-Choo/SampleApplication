88399d7233023d0fd5d27be4d16e4b9d6388fb48
JC-Choo <jicheolchoo@gmail.com>
2021-04-15 08:56:40 +0900

    Test viewmodel
    Signed-off-by: JC-Choo <jicheolchoo@gmail.com>

--- mvvmarchitecture/src/androidTest/java/dev/chu/mvvmarchitecture/ExampleInstrumentedTest.kt
+++ mvvmarchitecture/src/androidTest/java/dev/chu/mvvmarchitecture/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package dev.chu.mvvmarchitecture
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import dev.chu.mvvmarchitecture.api.Api
+import dev.chu.mvvmarchitecture.data.MainRepository
+import dev.chu.mvvmarchitecture.data.local.CatDatabase
+import dev.chu.mvvmarchitecture.data.local.LocalDataSource
+import dev.chu.mvvmarchitecture.data.remote.RemoteDataSource
+import dev.chu.mvvmarchitecture.ui.MainViewModel
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,46 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
+//    @Test
+//    fun useAppContext() {
+//        // Context of the app under test.
+//        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+//        assertEquals("dev.chu.mvvmarchitecture", appContext.packageName)
+//    }
+//
+//    @get:Rule
+//    val instantExecutorRule = InstantTaskExecutorRule()
+
+
+    private lateinit var database: CatDatabase
+    private lateinit var localDataSource: LocalDataSource
+    private lateinit var remoteDataSource: RemoteDataSource
+    private lateinit var repository: MainRepository
+    private lateinit var viewModel: MainViewModel
+
+    @Before
+    fun setUp() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("dev.chu.mvvmarchitecture", appContext.packageName)
+        database = CatDatabase.getInstance(appContext)
+        localDataSource = LocalDataSource(database)
+        remoteDataSource = RemoteDataSource(Api.createApi())
+        repository = MainRepository(localDataSource, remoteDataSource)
+        viewModel = MainViewModel(repository)
+    }
+
+    @Test
+    fun fetchData() {
+
+        runBlocking {
+            viewModel.fetchData { list ->
+                list?.let {
+                    viewModel.insert(it)
+                }
+            }
+
+            viewModel.getAllCats().value?.let {
+                assertTrue(it.isEmpty())
+            }
+        }
     }
 }
