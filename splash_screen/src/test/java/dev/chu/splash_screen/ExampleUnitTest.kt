6a650993f1931b3d7219790994239276ab456b3e
JC-Choo <jicheolchoo@gmail.com>
2021-05-31 08:48:47 +0900

    Test unit
    Signed-off-by: JC-Choo <jicheolchoo@gmail.com>

--- splash_screen/src/test/java/dev/chu/splash_screen/ExampleUnitTest.kt
+++ splash_screen/src/test/java/dev/chu/splash_screen/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package dev.chu.splash_screen
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,6 +9,38 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @Test
+    fun testCollectionOrSequence() {
+        val list00 = (1..10)
+            .filter {
+                println("filter $it")
+                it%2 == 1
+            }
+            .map {
+                println("map $it")
+                it*2
+            }
+            .take(2)
+        list00.forEach { print("$it ") }
+        println()
+        println("------------")
+
+        val list01 = (1..10).asSequence()
+            .filter {
+                println("filter $it")
+                it%2 == 1
+            }
+            .map {
+                println("map $it")
+                it*2
+            }
+            .take(2)
+            .toList()
+        list01.forEach { print("$it ") }
+        println()
+        println("------------")
+    }
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
