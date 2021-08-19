04043bc3f3cc7eef56bf5790e040477811ee1e24
jicheolchoo <jicheolchoo@gmail.com>
2021-08-19 18:16:40 +0900

    Add new rv test sample
    Signed-off-by: jicheolchoo <jicheolchoo@gmail.com>

---
+++ rv_flexible_mvvm/src/main/java/dev/chu/rv_flexible_mvvm/MainActivity.kt
@@ -0,0 +1,14 @@
+package dev.chu.rv_flexible_mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * https://proandroiddev.com/flexible-recyclerview-adapter-with-mvvm-and-data-binding-74f75caef66a
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
