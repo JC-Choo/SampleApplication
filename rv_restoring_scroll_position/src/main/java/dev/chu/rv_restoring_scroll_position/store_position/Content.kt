245d345e6e1ebd073358f660654e8d09e0bb9e21
JC-Choo <jicheolchoo@gmail.com>
2021-09-14 10:18:53 +0900

    Modify codes
    Signed-off-by: JC-Choo <jicheolchoo@gmail.com>

--- rv_restoring_scroll_position/src/main/java/dev/chu/rv_restoring_scroll_position/store_position/Content.kt
+++ rv_restoring_scroll_position/src/main/java/dev/chu/rv_restoring_scroll_position/store_position/Content.kt
@@ -1,5 +1,8 @@
 package dev.chu.rv_restoring_scroll_position.store_position
 
+/**
+ * viewHolder 별 item 에 상속받아야 하는 sealed class
+ */
 sealed class Content(val id: String) {
     class Banner(id: String) : Content(id)
     class Carousel(id: String, val list: List<Int>) : Content(id)
--- rv_restoring_scroll_position/src/main/java/dev/chu/rv_restoring_scroll_position/view/MainViewModel.kt
+++ rv_restoring_scroll_position/src/main/java/dev/chu/rv_restoring_scroll_position/view/MainViewModel.kt
@@ -17,17 +17,20 @@ class MainViewModel : ViewModel() {
 
     private fun generateContent() {
         val cards = listOf(1..8).flatten()
-        _content.value = listOf(
-            Content.Carousel("100", cards),
-            Content.Banner("1"),
-            Content.Carousel("101", cards),
-            Content.Carousel("102", cards),
-            Content.Carousel("103", cards),
-            Content.Banner("2"),
-            Content.Carousel("104", cards),
-            Content.Carousel("105", cards),
-            Content.Carousel("106", cards),
-            Content.Banner("3")
-        )
+        val carousels = listOf(1..100).flatten().map {
+            Content.Carousel((100+it).toString(), cards)
+        }
+        val banners = listOf(1..15).flatten().map {
+            Content.Banner(it.toString())
+        }
+
+        val contents = mutableListOf<Content>().apply {
+            addAll(carousels)
+            addAll(banners.subList(1, banners.size))
+            shuffle()
+            add(0, banners[0])
+        }
+
+        _content.value = contents
     }
 }
